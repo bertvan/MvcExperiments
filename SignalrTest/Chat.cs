7299493ea962936de6f5615341449f39f95ca43e
Bert Vanpeteghem <vanpeteghem.bert@gmail.com>
2012-01-25 22:51:34 +0100

    commit initial (naive) project

---
+++ SignalrTest/Chat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalrTest
+{
+    [HubName("chat")]
+    public class Chat : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.addMessage(message);
+        }
+
+        public void MousePosition(dynamic position)
+        {
+            var x = position["x"];
+            var y = position["y"];
+
+            Clients.setServerMousePosition(x, y);
+        }
+    }
+}
