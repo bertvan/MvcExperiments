dab22dceb5167bc518f5079bf0c70486b4efc1ec
Bert Vanpeteghem <vanpeteghem.bert@gmail.com>
2012-02-02 14:01:58 +0100

    tried to better integrate the change binding with knockout and signalR. However, it seems Chrome uses newlines as <div>'s which in turn don't trigger the change event... therefor, not such a good solution :(

--- Collab/Scripts/Site.js
+++ Collab/Scripts/Site.js
@@ -3,18 +3,42 @@
     var self = this;
     self.text = text;
     self.id = id;
+
+    self.save = function (text) {
+
+    	// Call the paragraphHub method on the server
+    	// improvement needed, KnockoutJS binding does not seem to work that way, so fetching myself here :s
+    	// so I actually should be able to use paragraph.text ... ?
+    	var paragraphHub = $.connection.paragraphHub;
+    	paragraphHub.send(
+        {
+        	id: self.id,
+        	text: text
+        });
+    };
 }
 
-function ParagraphEditingViewModel(){
-    this.paragraphs = ko.observableArray(
+function ParagraphEditingViewModel() {
+
+	var self = this;
+
+    self.paragraphs = ko.observableArray(
     [
         new Paragraph(0, "bla"),
         new Paragraph(1, "foo"),
         new Paragraph(2, "hihihi")
     ]);
+
+    self.updateParagraphs = function (data) {
+    	// data received in viewmodel
+    	var mapped = $.map(data, function (item) { return new Paragraph(item.Id, item.Text) });
+    	self.paragraphs(mapped);
+	}
 }
 
 function initParagraphEditing() {
+
+	var model = new ParagraphEditingViewModel();
 
     // signalR (comments not mine, but from an original sample I used
     // over at: http://www.dreamincode.net/forums/blog/1267/entry-3681-signalr-with-mvc3-chat-app-build-asynchronous-real-time-persistant-connection-websites/
@@ -24,39 +48,20 @@ function initParagraphEditing() {
 
     // Declare a function on the chat hub so the server can invoke it
     paragraphHub.reloadParagraphs = function (paragraphs) {
-        //alert('go go go'); 
-        // TODO, more logic needs to go into the ViewModel, to callback on it when the 
-        // data is reloaded: see example on http://learn.knockoutjs.com/#/?tutorial=loadingsaving
+    	model.updateParagraphs(paragraphs);
+    	// TODO, more logic needs to go into the ViewModel, to callback on it when the 
+    	// data is reloaded: see example on http://learn.knockoutjs.com/#/?tutorial=loadingsaving
     };
 
     // Start the connection
     $.connection.hub.start();
 
-    // knockout
-    ko.applyBindings(new ParagraphEditingViewModel());
-
     // trigger actions on paragraph change
     initChangeDetection();
+	
+    // knockout
+    ko.applyBindings(model);
 
-    // trigger signalR from paragraph change event
-    $('.paragraph-text').live('change', function () {
-        var paragraph = ko.dataFor(this);
-
-//        $.post('/Home/ProcessParagraph',
-//        {
-//            id: 0,
-//            text: paragraph.text
-//        }, function (data) {
-//            // nothing for now
-        //        });
-
-        // Call the paragraphHub method on the server
-        paragraphHub.send(
-        {
-            id: 0,
-            text: $(this).html() // improvement needed, KnockoutJS binding does not seem to work that way, so fetching myself here :s
-        });
-    });
 }
 
 function initChangeDetection() {
@@ -72,6 +77,14 @@ function initChangeDetection() {
             $this.trigger('change');
         }
         return $this;
+    });
+
+    // trigger viewmodel paragraph change event
+	// again too bad we can't make this happen with binding
+    $('.paragraph-text').live('change', function () {
+    	var paragraph = ko.dataFor(this);
+    	var text = $(this).html();
+    	paragraph.save(text);
     });
 
 }
