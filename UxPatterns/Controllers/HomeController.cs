a38d6adcc7796dee16d156db26df45100d79ca52
Bert Vanpeteghem <vanpeteghem.bert@gmail.com>
2012-02-14 12:01:19 +0100

    added uxpatterns project

---
+++ UxPatterns/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UxPatterns.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+
+		public ActionResult Birds(string term)
+		{
+			var birds = new [] { "Vink", "Mus", "Papegaai", "Test", "Ooievaar", "Parkiet" };
+
+			var matchingBirds = birds
+				.Where(x => x.ToLowerInvariant().Contains(term.ToLowerInvariant()))
+				.ToArray();
+
+			return Json(matchingBirds, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
